0f0fea2d234a690ba7cedafccf59bdea804ca171
Ammar Ahmed <ammar7298@gmail.com>
2025-06-03 03:53:33 -0700

    spline robot added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact,  Hero, Navbar,  Works, StarsCanvas } from "./components";
+import SplineScene from "./components/SplineScene";
 const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+    <SplineScene/>
           <Navbar />
           <Hero />
         </div>
